46eb55df16e26a74442b25195aa2afe97fbe5c37
OSORIO SOAREZ <153106190+SoarezTL@users.noreply.github.com>
2026-04-17 05:09:59 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return (
+    <ClerkProvider>
+      {children}
+    </ClerkProvider>
+  )
 }
